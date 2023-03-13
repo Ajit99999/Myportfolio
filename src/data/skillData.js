5494711602789a5d541b1e2794505dd22bdd27a9
Ajit Sahoo <87249806+Ajit99999@users.noreply.github.com>
2023-03-13 18:51:10 +0530

    Create skillData.js

---
+++ src/data/skillData.js
@@ -0,0 +1,59 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiRedux,
+  SiFirebase,
+  SiNodedotjs,
+  SiExpress,
+  SiJavascript,
+} from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+
+export const skillData = [
+  {
+    imageUrl: SiHtml5,
+    title: "HTML",
+    id: 1,
+  },
+  {
+    imageUrl: SiCss3,
+    title: "CSS",
+    id: 2,
+  },
+  {
+    imageUrl: SiJavascript,
+    title: "JavaScript",
+    id: 10,
+  },
+  {
+    imageUrl: FaReact,
+    title: "React",
+    id: 3,
+  },
+  {
+    imageUrl: SiRedux,
+    title: "Redux",
+    id: 4,
+  },
+  {
+    imageUrl: SiFirebase,
+    title: "FireBase",
+    id: 5,
+  },
+  {
+    imageUrl: SiNodedotjs,
+    title: "Node.Js",
+    id: 6,
+  },
+  {
+    imageUrl: SiExpress,
+    title: "Express",
+    id: 7,
+  },
+  {
+    imageUrl: BsGithub,
+    title: "GitHub",
+    id: 8,
+  },
+];
